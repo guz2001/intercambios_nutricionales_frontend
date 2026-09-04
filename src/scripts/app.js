17eaa989a92135b0c13c686a6f2a64ef48958a0f
guz2001 <guz20dgc@gmail.com>
2026-09-04 12:25:11 -0500

    funcion math.roun para redondear el numero de la porcion y no tener ese voleo de numeros

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -137,10 +137,11 @@ function renderizarDetalle(alimento) {
 
   /* Métricas superiores */ 
   setText('val-kcal', Math.round(alimento.kcal));/*Math round redondea el numero al entero mas cercano */
-  setText('val-porcion', `${alimento.porcion_g} `);
+  setText('val-porcion', `${Math.round(alimento.porcion_g)}g`);
   setText('val-unidad',`${alimento.unidad_medida}`);
   setText('val-proteina', `${alimento.proteina_g}g`);
   setText('val-grasa', `${alimento.grasa_total_g}g`);
+  setText('val-poblacion', `${alimento.poblacion}`);
 
   /* Macronutrientes */
   setText('val-proteina-det', formatearValor(alimento.proteina_g, 'g'));
